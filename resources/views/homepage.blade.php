a8edd5a661e640df84ec0d9eee49b7868cbe8fc6
Muhammad Nuril Muqit <nurilmuqit@gmail.com>
2024-10-28 03:01:13 +0800

    feat: :sparkles: show product section
    new section

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -12,7 +12,7 @@
 
 </head>
 
-<body>
+<body class="font-montserrat">
 
     {{-- Navbar --}}
     <header class="bg-white">
@@ -20,10 +20,10 @@
             <div class="flex items-center ml-5">
                 <a href="#">
                     {{-- ganti ke logo --}}
-                    <h1 class="font-montserrat font-bold">LOGO</h1>
+                    <h1 class=" font-bold">LOGO</h1>
                 </a>
             </div>
-            <div class="flex flex-row space-x-32 text-putihsusu font-semibold font-montserrat mr-20">
+            <div class="flex flex-row space-x-32 text-putihsusu font-semibold mr-20">
                 <a href="#home" class="hover:text-gray-300">Home</a>
                 <a href="#products" class="hover:text-gray-300">Products</a>
                 <a href="#about" class="hover:text-gray-300">About Us</a>
@@ -36,9 +36,10 @@
         style="background-image: url('{{ asset('images/dummy.png') }}');">
         <div class="container mx-auto text-center">
             {{-- ganti ke logo --}}
-            <h1 class="font-montserrat font-bold">LOGO</h1>
+            <h1 class="font-bold">LOGO</h1>
             <h1 class="text-biru font-bold text-5xl text-shadow-xl mb-2">Celebes Ocean Fisheries</h1>
-            <p class="text-biru font-semibold text-lg max-w-6xl mx-auto">Celebes Ocean Fisheries adalah sebuah perusahaan Lorem Ipsum has
+            <p class="text-biru text-lg max-w-6xl mx-auto">Celebes Ocean Fisheries adalah sebuah
+                perusahaan Lorem Ipsum has
                 been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of
                 type and scrambled it to make a type specimen book. It has survived not only five centuries, but also
                 the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s
@@ -46,6 +47,33 @@
                 publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
         </div>
     </section>
+
+    {{-- Products --}}
+    <section id="products" class="bg-white py-20">
+        <div class="container mx-auto text-center">
+
+            <h1 class="text-biru font-bold text-5xl mb-5 text-shadow-xl">Products</h1>
+            <p class="text-biru text-lg mb-5 max-w-7xl mx-auto">Celebes Ocean Fisheries memiliki spesialisasi di bidang
+                Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer
+                took a galley of type and scrambled it to make a type specimen book</p>
+
+            <div class="grid grid-cols-1 md:grid-cols-3 gap-8 m-5">
+                <div class="bg-gray-100 rounded-lg shadow-lg p-6 flex flex-col items-center">
+                    <img src="{{ asset('images/dummy.png') }}" alt="product" class="object-cover rounded-lg">
+                    <h1 class="text-blue-500 text-lg mt-4 text-center">Product No 1</h1>
+                </div>
+                <div class="bg-gray-100 rounded-lg shadow-lg p-6 flex flex-col items-center">
+                    <img src="{{ asset('images/dummy.png') }}" alt="product" class="object-cover rounded-lg">
+                    <h1 class="text-blue-500 text-lg mt-4 text-center">Product No 2</h1>
+                </div>
+                <div class="bg-gray-100 rounded-lg shadow-lg p-6 flex flex-col items-center">
+                    <img src="{{ asset('images/dummy.png') }}" alt="product" class="object-cover rounded-lg">
+                    <h1 class="text-blue-500 text-lg mt-4 text-center">Product No 3</h1>
+                </div>
+            </div>
+
+
+        </div>
 
 </body>
 
